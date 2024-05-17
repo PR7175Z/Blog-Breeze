ae7a2a9fe5323509353d043c9f10e6abbf3504a2
PR7175Z <aadimpraz@gmail.com>
2024-05-17 09:25:03 +0545

    added image upload feature

--- blogbreezeapp/models.py
+++ blogbreezeapp/models.py
@@ -6,6 +6,7 @@ class Blog(models.Model):
   title = models.CharField(max_length=255)
   publishdate = models.DateTimeField()
   authorid = models.IntegerField(default=0)
+  featuredimage = models.ImageField(upload_to ='uploads/') 
 
   def __str__(self):
     return f'{self.title}'
