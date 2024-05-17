63684d3bd4f5c6a4052f484b0f37778b6ffa895e
PR7175Z <aadimpraz@gmail.com>
2024-05-17 10:53:34 +0545

    added image but no no link

--- blogbreezeapp/models.py
+++ blogbreezeapp/models.py
@@ -6,7 +6,7 @@ class Blog(models.Model):
   title = models.CharField(max_length=255)
   publishdate = models.DateTimeField()
   authorid = models.IntegerField(default=0)
-  featuredimage = models.ImageField(upload_to ='uploads/') 
+  featuredimage = models.ImageField(upload_to ='uploads/', default=0) 
 
   def __str__(self):
     return f'{self.title}'
