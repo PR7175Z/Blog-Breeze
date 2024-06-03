257f677ba40485023bc0e61fd146ab62e203fb7c
PR7175Z <aadimpraz@gmail.com>
2024-06-03 22:51:14 +0545

    created sign in and signup page

--- blogbreezeapp/models.py
+++ blogbreezeapp/models.py
@@ -6,6 +6,7 @@ import uuid
 class Category(models.Model):
   name = models.CharField(max_length=30)
   content = HTMLField(default = '')
+  author = models.ForeignKey(User, on_delete=models.CASCADE, default='')
 
   def __str__(self):
     return f'{self.name}'
@@ -14,7 +15,7 @@ class Blog(models.Model):
   title = models.CharField(max_length=255)
   slug = models.SlugField(default="", null=False)
   publishdate = models.DateTimeField(auto_now_add=True)
-  authorid = models.IntegerField(default=0)
+  authorid = models.ForeignKey(User, on_delete=models.CASCADE, default='')
   featuredimage = models.ImageField(upload_to ='uploads/', default=0) 
   content = HTMLField(default='')
   category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='blogs', default=1)
